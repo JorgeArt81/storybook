7056c84ec569acfe6b379fb1fab1139cd1fddc84
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2023-03-31 19:04:23 -0500

    story demo

---
+++ src/components/Demo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {
+  label: string;
+  size: 'small' | 'medium' | 'large';
+}
+export const Demo = () => {
+  return <h1>Hola Mundo</h1>;
+};
---
+++ src/stories/components/Demo.stories.tsx
@@ -0,0 +1,12 @@
+import { Demo } from '../../components/Demo';
+
+export default {
+  title: 'UI/Demo',
+  component: Demo,
+};
+
+const Template = () => <Demo />;
+
+export const Basic = Template.bind({});
+export const AllCaps = Template.bind({});
+export const Secondary = Template.bind({});
